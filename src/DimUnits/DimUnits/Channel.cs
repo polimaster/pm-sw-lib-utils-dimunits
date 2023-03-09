d78d8ccab3237915e2dcd4bdac35b18ea6232f47
Voronische <voronische@polimaster.eu>
2023-03-09 15:26:45 +0200

    test github actions

--- src/DimUnits/DimUnits/Channel.cs
+++ src/DimUnits/DimUnits/Channel.cs
@@ -3,16 +3,8 @@ using System.Collections.Generic;
 using System.Threading;
 using Polimaster.Utils.DimUnits.res;
 
-namespace Polimaster.Utils.DimUnits{
-    public enum ChannelCode{
-        NONE = 0,
-        ALPHA = 1,
-        GAMMA = 2,
-        BETA = 3,
-    }
-
-    public struct Channel
-    {
+namespace Polimaster.Utils.DimUnits {
+    public struct Channel {
         public ChannelCode Code;
         public byte CodeValue => (byte)Code;
 
@@ -26,22 +18,24 @@ namespace Polimaster.Utils.DimUnits{
         /// Returns localized label.
         /// </summary>
         public override string ToString() {
-            return Resources.ResourceManager.GetString(Enum.GetName(typeof(ChannelCode), Code), Thread.CurrentThread.CurrentCulture);
+            return Resources.ResourceManager.GetString(Enum.GetName(typeof(ChannelCode), Code),
+                Thread.CurrentThread.CurrentCulture);
         }
-
     }
 
-    public static class Channels{
+    public static class Channels {
         private static List<Channel> _LIST;
+
         /// <summary>
         /// List of avaliable channels (ChannelCode.NONE channel is not included).
         /// </summary>
-        public static List<Channel> LIST{
-            get{
-                if(_LIST == null){
+        public static List<Channel> LIST {
+            get {
+                if (_LIST == null) {
                     _LIST = new List<Channel>();
-                    foreach(var channel in DICTIONARY) _LIST.Add(channel.Value);
+                    foreach (var channel in DICTIONARY) _LIST.Add(channel.Value);
                 }
+
                 return _LIST;
             }
         }
@@ -49,11 +43,11 @@ namespace Polimaster.Utils.DimUnits{
         /// <summary>
         /// Dictionary of avaliable channels (ChannelCode.NONE channel is not included).
         /// </summary>
-        public static readonly Dictionary<ChannelCode, Channel> DICTIONARY = new Dictionary<ChannelCode, Channel>{
-            {ChannelCode.ALPHA, new Channel{Code = ChannelCode.ALPHA}},
-            {ChannelCode.BETA, new Channel{Code = ChannelCode.BETA}},
-            {ChannelCode.GAMMA, new Channel{Code = ChannelCode.GAMMA}},
-            {ChannelCode.NONE, new Channel{Code = ChannelCode.NONE}}
+        public static readonly Dictionary<ChannelCode, Channel> DICTIONARY = new Dictionary<ChannelCode, Channel> {
+            { ChannelCode.ALPHA, new Channel { Code = ChannelCode.ALPHA } },
+            { ChannelCode.BETA, new Channel { Code = ChannelCode.BETA } },
+            { ChannelCode.GAMMA, new Channel { Code = ChannelCode.GAMMA } },
+            { ChannelCode.NONE, new Channel { Code = ChannelCode.NONE } }
         };
     }
 }
---
+++ src/DimUnits/DimUnits/ChannelCode.cs
@@ -0,0 +1,8 @@
+namespace Polimaster.Utils.DimUnits;
+
+public enum ChannelCode {
+    NONE = 0,
+    ALPHA = 1,
+    GAMMA = 2,
+    BETA = 3,
+}
